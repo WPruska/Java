69235f2c5590aa3bc817a18e1e4bd5ad854b24f3
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2022-03-13 00:07:40 +0100

    Zad 2 update

--- Zad2/src/Main.java
+++ Zad2/src/Main.java
@@ -18,13 +18,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        int start,end,text;
+        int start,end;
+        String text;
+
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Give text: ");
+        text =  scan.nextLine();
         System.out.println("Give first number of string: ");
         start = scan.nextInt();
         System.out.println("Give end number of string: ");
         end = scan.nextInt();
-        System.out.println("Give text ");
+
+        if(start < 0 || end > text.length())
+        {
+            System.out.printf("Bad given index");
+            return;
+        }
+
+        String stringToDisplay = text.substring(start,end);
+        System.out.println(stringToDisplay);
     }
 }
